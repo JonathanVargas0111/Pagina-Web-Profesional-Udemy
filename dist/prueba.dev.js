5cfe1ab9db0787947e0e23d881b8c422a828807d
Jonathan Vargas <jonathanfelipe0111@hotmail.com>
2020-09-17 18:12:03 -0500

    Realizando sección de contenido Parte 2

---
+++ dist/prueba.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var index = 1;
+var producto = [1, 2];
+var valorColArray = {
+  indice: index,
+  producto: producto(index),
+  valorCol: valorCol
+};
+console.log(valorColArray[index]);
